1c5838c16375f5113bd4f182a4cac92ad849a3d3
Nicolas Gurfinkel <nicolasgurfi123@gmail.com>
2026-03-27 10:08:27 -0300

    cambios cron mas seguidos

--- services/order_service.py
+++ services/order_service.py
@@ -102,7 +102,7 @@ class OrderSyncService:
             
             print(ordenes_creadas, ordenes_no_creadas)
             html_message = generar_html_reporte_creacion_ordenes(ordenes_creadas, ordenes_no_creadas)
-            enviar_reporte_email(html_message, ["ngurfinkel@the5411.com", "sguaita@the5411.com", ], "Órdenes TT Dsco")
+            enviar_reporte_email(html_message, ["ngurfinkel@the5411.com", "sguaita@the5411.com" ], "Órdenes TT Dsco")
                     #
 
             #Avisar por mail las ordenes creadas con exito y sino el error 
