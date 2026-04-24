59f3b08b84aa37acc518f96116a4b4e50e7d947d
Nicolas Gurfinkel <nicolasgurfi123@gmail.com>
2026-04-24 11:13:31 -0300

    cambio mail envio

--- services/order_service.py
+++ services/order_service.py
@@ -202,8 +202,9 @@ class OrderSyncService:
                     ordenes_no_creadas.append(mapped_order["OrderNumber"])
             
             print(ordenes_creadas, ordenes_no_creadas, info_ordenes)
-            html_message = generar_html_reporte_creacion_ordenes(ordenes_creadas, ordenes_no_creadas, info_ordenes)
-            enviar_reporte_email(html_message, ["ngurfinkel@the5411.com" , "jvillanueva@the5411.com"], "Órdenes Shirty Dsco") # "sguaita@the5411.com",
+            if ordenes_creadas or ordenes_no_creadas:
+                html_message = generar_html_reporte_creacion_ordenes(ordenes_creadas, ordenes_no_creadas, info_ordenes)
+                enviar_reporte_email(html_message, ["ngurfinkel@the5411.com" , "jvillanueva@the5411.com"], "Órdenes Shirty Dsco") # "sguaita@the5411.com",
             scrollId = dsco_orders_1.get("scrollId")
             if scrollId:
                 carga_larga = self.carga_larga(scrollId)
