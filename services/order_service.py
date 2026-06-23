7aa16e81f9c0bade75a7dd8eccfb18c4af1eb822
Nicolas Gurfinkel <nicolasgurfi123@gmail.com>
2026-06-23 10:40:57 -0300

    cambios sm

--- services/order_service.py
+++ services/order_service.py
@@ -42,23 +42,15 @@ class OrderSyncService:
             mintsoft_order = next((order for order in mintsoft_orders if order["OrderNumber"] == dsco_order["poNumber"]), None)
             if not mintsoft_order:
                 created.append(dsco_order)
-            elif datetime.fromisoformat(dsco_order["dscoLastUpdate"]) > datetime.fromisoformat(mintsoft_order["ConnectAction"]["ExtraDate1"]):
-                updated.append({"order":dsco_order, "mintsoft_id": mintsoft_order["Id"]})
-            else:
-                skipped.append(dsco_order)
+            
 
         self.logger.info(f"Orders to be created: {len(created)}")
-        self.logger.info(f"Orders to be updated: {len(updated)}")
-        self.logger.info(f"Orders to be skipped: {len(skipped)}")
 
         for order in created:
             mapped_order = map_dsco_order(order)
             self.mintsoft_client.create_order(mapped_order)
 
-        for order in updated:
-            mapped_order = map_dsco_order(order.get("order"))
-            order_items = mapped_order.pop("OrderItems")
-            self.mintsoft_client.update_order(mapped_order, order.get("mintsoft_id"))
+        
 
         with open('./state/state.json', 'r') as file:
             states = json.load(file)
